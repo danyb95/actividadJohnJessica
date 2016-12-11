ed27614918d06154d75cda03022fa505ba7605ac
danyb <danyb@DESKTOP-FSK5P95>
2016-12-11 15:16:41 +0100

    Versión John 1.1

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO2345");    
+        System.out.println("HOLA MUNDO23456");    
     }
     
 }
